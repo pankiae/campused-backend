d8ea95ca6d78ccb568434f72217c1adf59812c21
dev-jarial <pankajjarial.dev@gmail.com>
2025-10-08 01:49:20 +0530

    rename the project to campused

--- campused/asgi.py
+++ campused/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for skelton project.
+ASGI config for campused project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skelton.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campused.settings")
 
 application = get_asgi_application()

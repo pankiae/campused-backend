15bd776804958532c51909acc94b8b1132e93490
dev-jarial <pankajjarial.dev@gmail.com>
2025-10-12 09:24:29 +0530

    gpt model updated to gpt-5-nano-2025-08-07

--- utils/openai_logic/text_generation.py
+++ utils/openai_logic/text_generation.py
@@ -1,10 +1,6 @@
 from utils.openai_logic.client_create import client
 
 
-def text_generation(q): # q = query
-    response = client.responses.create(
-    model="gpt-5",
-    input=q
-)
+def text_generation(q):  # q = query
+    response = client.responses.create(model="gpt-5-nano-2025-08-07", input=q)
     return response.output_text
-

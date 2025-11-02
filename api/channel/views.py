0832b4ad7eb3852285086808d915d8b79a2071fe
dev-jarial <pankajjarial.dev@gmail.com>
2025-11-02 23:52:53 +0530

    + attached the token to th request in patch api

--- api/channel/views.py
+++ api/channel/views.py
@@ -315,7 +315,8 @@ class PatchChannelView(APIView):
             request.user,
             len(conversation),
         )
-
+        request._request.gather_tokens = gather_tokens
+        request._request.gather_tokens["model"] = select_openai_model
         return Response(
             {"conversation": conversation},
             status=200,
